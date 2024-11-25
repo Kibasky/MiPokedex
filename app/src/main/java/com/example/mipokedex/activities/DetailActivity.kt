623178febc4dc3e7c1a6fc514323f9777a03d970
Damivk <damianpemo@gmail.com>
2024-11-25 19:21:02 +0100

    añadimos el permiso a internet en Manifest. cambiamos el gradle a 35. añadimos binding en gradle, y lo llamamos en cada activity. creamos el item_pokemon principal

--- app/src/main/java/com/example/mipokedex/activities/DetailActivity.kt
+++ app/src/main/java/com/example/mipokedex/activities/DetailActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mipokedex.R
+import com.example.mipokedex.databinding.ActivityDetailBinding
 
-class DetailsActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_details)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/mipokedex/activities/ListActivity.kt
+++ app/src/main/java/com/example/mipokedex/activities/ListActivity.kt
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mipokedex.R
+import com.example.mipokedex.databinding.ActivityListBinding
 
 class ListActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
